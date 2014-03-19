b3653f54ecbb23626f9d66b90e2a7ac7d3d20fe4
Alberto Pedron <albertop2197@gmail.com>
2014-03-20 00:50:00 +0100

    Update README.m

--- README.m
+++ README.m
@@ -5,5 +5,4 @@ For D-Link DWA-140 and other Ralink 2870 chips [Linux-Ubuntu]
 
 rt2870.bin -> Move to lib/firmware  (permissions: chmod 644) -> Firmware version 22
 
-
 rt2870     -> Move to etc/init.d    (permissions: chmod 755) -> Script to make network faster
