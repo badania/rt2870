9e72e38a94c3d45a581d4e55fb05f60aafd66f81
Alberto Pedron <albertop2197@gmail.com>
2014-03-20 00:49:48 +0100

    Update and rename README.md to README.m

--- README.m
+++ README.m
@@ -4,4 +4,6 @@ D-Link RT2870
 For D-Link DWA-140 and other Ralink 2870 chips [Linux-Ubuntu]
 
 rt2870.bin -> Move to lib/firmware  (permissions: chmod 644) -> Firmware version 22
+
+
 rt2870     -> Move to etc/init.d    (permissions: chmod 755) -> Script to make network faster
